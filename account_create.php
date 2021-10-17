2f3862b31d39dc5917c83e6c26aec328030ae54f
Chui Tey <ctey@emlpayments.com>
2021-10-17 14:09:40 +0000

    Create account

--- account_create.php
+++ account_create.php
@@ -18,7 +18,10 @@
           <text style="Header__Title">Better Grip</text>
       </header>
       <view style="Container">
-
+        <text>Based on the Open Banking Data, you are</text>
+        <text>Seong Gi-hun</text>
+        <text style="Button Link">Yes, that is me. Create my account</text>
+        <text style="Button Link" href="/index.php" action="navigate">Back to home</text>
       </view>
       <view
         href="#"
